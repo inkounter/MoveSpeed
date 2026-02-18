d433d78cd2ac934b9a253be83d0b325bd308a304
Inkounter <inkounter@gmail.com>
2026-02-18 00:15:59 -0500

    Integrate with `LibEditMode`.

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
 local thisAddonName, ns = ...
+local LibEditMode = LibStub("LibEditMode")
 
-local frame = CreateFrame("Frame", nil, UIParent)
+local frame = CreateFrame("Frame", "MoveSpeedFrame", UIParent)
+frame:SetSize(40, 20)
+
 local fontstring = frame:CreateFontString(nil, "OVERLAY", "GameFontWhite")
-fontstring:SetPoint("CENTER", UIParent, "CENTER")
+fontstring:SetPoint("RIGHT")
+fontstring:SetJustifyH("RIGHT")
 
 local function getSpeed()
     local isGliding, _, speed = C_PlayerInfo.GetGlidingInfo()
@@ -17,12 +21,33 @@ local function updateSpeed()
     fontstring:SetFormattedText("%.0f%%", getSpeed())
 end
 
+local defaultPosition = {point = "RIGHT", x = 0, y = 0}
+
+local function onPositionChanged(f, layoutName, point, x, y)
+    MoveSpeedDB = MoveSpeedDB or {}
+    MoveSpeedDB[layoutName] = MoveSpeedDB[layoutName] or {}
+    MoveSpeedDB[layoutName].point = point
+    MoveSpeedDB[layoutName].x = x
+    MoveSpeedDB[layoutName].y = y
+end
+
+LibEditMode:RegisterCallback(
+    "layout",
+    function(layoutName)
+        local pos = MoveSpeedDB and MoveSpeedDB[layoutName] or defaultPosition
+        frame:ClearAllPoints()
+        frame:SetPoint(pos.point, UIParent, pos.point, pos.x, pos.y)
+    end
+)
+
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript(
     "OnEvent",
     function(_frame, _event, addonName)
         if addonName == thisAddonName then
             frame:UnregisterEvent("ADDON_LOADED")
+
+            LibEditMode:AddFrame(frame, onPositionChanged, defaultPosition)
 
             C_Timer.NewTicker(0.1, updateSpeed)
         end
